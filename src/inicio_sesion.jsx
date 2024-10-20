72b57713e50db18379da164d458661410df74181
Juan <juancruzm2004@gmail.com>
2024-10-19 21:52:43 -0300

    json server

---
+++ src/inicio_sesion.jsx
@@ -0,0 +1,13 @@
+const Inicio_sesion = () =>{
+    return (
+        <>
+            <p>Iniciar sesion</p>
+
+            <button type="button" className="btn">iniciar</button>
+        
+        </>
+    )
+
+}
+
+export default Inicio_sesion
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Inicio_sesion from './inicio_sesion'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Inicio_sesion/>
   </StrictMode>,
 )
