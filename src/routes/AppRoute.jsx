db1ea4306483177fb098d27d5704118ec2308df0
Gmontemarani <gianfrancomonte2@gmail.com>
2024-11-09 21:55:22 -0300

    Gestion de productos + Catalogo por categorias

--- src/routes/AppRoute.jsx
+++ src/routes/AppRoute.jsx
@@ -3,7 +3,9 @@ import Home from '../pages/Home';
 import Header from '../components/Header.jsx';
 import Login from '../pages/IniciarSesion.jsx';
 import Register from '../pages/CrearCuenta.jsx';
+import Productos from '../pages/Productos.jsx'
 import { useState } from 'react';
+import GestionProductos from '../pages/GestionProductos.jsx';
 
 function AppRoute() {
   const [isLoggedIn, setisLoggedIn] = useState(false);
@@ -23,6 +25,8 @@ function AppRoute() {
           <Route path="/" element={<Home isLoggedIn = {isLoggedIn}/>} />
           <Route path="/login" element={<Login handleLogin = {handleLogin}/>} />
           <Route path="/register" element={<Register handleLogin = {handleLogin} />} />
+          <Route path="/productos" element={<Productos isLoggedIn = {isLoggedIn} />} />
+          <Route path="/gestionProductos" element={<GestionProductos isLoggedIn = {isLoggedIn} />} />
       </Routes>
     </>
   )
